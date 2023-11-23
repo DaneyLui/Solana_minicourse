b7a48f819e5886ff742b5bbe8b90809e3f64fd52
Daney <18003812@es.uveg.edu.mx>
2023-11-23 22:32:10 +0800

    Update WalletConnectProvider.js

--- components/WalletConnectProvider.js
+++ components/WalletConnectProvider.js
@@ -4,3 +4,13 @@ import { WalletModalProvider} from "@solana/wallet-adapter-react-ui";
 import { GlowWalletAdapter, PhantomWalletAdapter, SlopeWalletAdapter} from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl } from "@solana/web3.js";
 import { useMemo } from "react";
+
+export const WalletConnectProvider = ({children}) =>{
+    const network = WalletAdapterNetwork.Devnet;
+    const endpoint = useMemo (() =>{
+        if(network == WalletAdapterNetwork.Devnet){
+            return "https://radial-indulgent-tree.solana-devnet.quiknode.pro/a7b4e28c0a4b6c012f9e9986cd1725dabe315d78/";
+        }
+        return clusterApiUrl(network);
+    },[network]);
+}
