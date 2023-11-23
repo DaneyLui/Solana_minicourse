b480ff39ff636a83121d232c825d185a03adb947
Daney <18003812@es.uveg.edu.mx>
2023-11-23 22:08:49 +0800

    Update WalletConnectProvider.js

---
+++ components/WalletConnectProvider.js
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+=======
+import {WalletAdapterNetwork} from "@solana/wallet-adapter-base";
+import {ConnectionProvider, WalletProvider} from "@solana/wallet-adapter-react-ui";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {GlowWalletAdapter, PhantomWalletAdapter, SlopeWalletAdapter} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+>>>>>>> parent of 540e9db (Update WalletConnectProvider.js)
